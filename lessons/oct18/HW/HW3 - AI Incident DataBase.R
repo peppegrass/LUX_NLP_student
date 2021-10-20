e38d4ef6106a2c4710aecc8dda6394b04fbc0dc5
Giuseppe Grasso <peppegrass@gmail.com>
2021-10-20 15:38:38 +0200

    beginning HW3

--- lessons/oct18/HW/HW3 - AI Incident DataBase.R
+++ lessons/oct18/HW/HW3 - AI Incident DataBase.R
@@ -1,5 +1,5 @@
 #' Title: Homework III
-#' Purpose: Obtain Data, save it, perform word frequency, then compare shared word frequencies
+#' Purpose: Perform sentiment analysis and cluster analysis
 #' Author: Giuseppe Grasso
 #' email: giuseppegrasso.econ@gmail.com
 #' License: GPL>=3
@@ -10,6 +10,9 @@
 
 ## Setting the working directory
 setwd("~/Documents/GitHub/LUX_NLP_student/lessons/oct18/HW")
+
+## options
+options(scipen = 999, stringsAsFactors = F)
 
 ## Libraries
 library(skmeans)
@@ -25,5 +28,20 @@ library(radarchart)
 library(ggplot2)
 library(ggthemes)
 
+# Custom Functions
+source('~/Documents/GitHub/LUX_NLP_student/lessons/Z_otherScripts/ZZZ_supportingFunctions.R')
 
+# Examine Raw Text
+## incidents_dates.csv
+incidents_dates <- read.csv('incident_dates.csv')
+incidents_dates$incidents_dates_fmt <- as.POSIXct(incidents_dates$Incident.Date, format = "%m/%d/%y")
+incidents_dates$Year <- format(as.Date(incidents_dates$incidents_dates_fmt, format="%d/%m/%y"),"%Y")
+incidents_dates <- incidents_dates[,c("incident_ID", "Incident.Date", "Year", "Citation", "Report.Count")]
+t(incidents_dates[1,])
+## incidents.csv
+incidents <- read.csv('incidents.csv')
+t(incidents[1:14,])
+
+inc <- full_join(incidents,incidents_dates, by="incident_ID")
+View(inc)
 # End
--- lessons/oct19/scripts/G_text2vec.R
+++ lessons/oct19/scripts/G_text2vec.R
@@ -8,7 +8,7 @@
 #'
 
 # WD
-setwd("~/Desktop/LUX_NLP_student/lessons/oct19/data")
+setwd("~/Documents/GitHub/LUX_NLP_student/lessons/oct19/data")
 
 # Libs
 library(data.table)
@@ -20,18 +20,18 @@ library(text2vec)
 testing <- F
 
 # Bring in our supporting functions
-source('~/Desktop/LUX_NLP_student/lessons/Z_otherScripts/ZZZ_supportingFunctions.R')
-airbnb <- readr::read_csv('Airbnb-boston_only.zip')
-names(airbnb)
+source('~/Documents/GitHub/LUX_NLP_student/lessons/Z_otherScripts/ZZZ_supportingFunctions.R')
+airbnb <- readr::read_csv('Airbnb-boston_only.zip') #GG: loading smaller dataset (only Boston), unzips the file as it opens it
+names(airbnb) #GG: data from Airbnb reviews
 
 # Make appropriate for tm
-airbnbTxt <- data.table(doc_id = airbnb$listing_id, 
+airbnbTxt <- data.table(doc_id = airbnb$listing_id, #GG: keeping only vars that we need, and renaming them. can retrieve further variables later if needed
                         text = airbnb$comments,
                         reviewScores = airbnb$review_scores_rating)
-rm(airbnb)
+rm(airbnb) #GG: detaching full file
 gc()
 
-# Apply standard cleaning
+# Apply standard cleaning #GG: These steps take a while so I've saved the rds and commented out some linew
 #txt <- VCorpus(DataframeSource(airbnbTxt))
 #txt <- cleanCorpus(txt, c(stopwords('en'), 'boston'))
 #saveRDS(txt,'txt.rds')
@@ -47,62 +47,65 @@ if(testing == T){
   n <- length(txtTokens)
 }
 iter <- itoken(txtTokens[1:n],
-            tokenizer = word_tokenizer, n_chunks = 10)
-nGramVocab <- create_vocabulary(iter, c(1, 3) )
+            tokenizer = word_tokenizer, n_chunks = 10) #GG: perform iterations of tokenizer in chunks
+nGramVocab <- create_vocabulary(iter, c(1, 3) ) #number of n-grams that we want: unigram, bigram, trigram: will be huge sparse matrix
 
 # Examine
 head(nGramVocab, 8)
 
 # Heuristic prune
-nGramVocab <- prune_vocabulary(nGramVocab, term_count_min = 5)
+nGramVocab <- prune_vocabulary(nGramVocab, term_count_min = 5) # GG: wanna chop off the tail if term count less then 5
 
 # Create the custom function 
 tokenVecs <- vocab_vectorizer(nGramVocab)
 
 # Create a DTM if needed
-tmDTM <-  create_dtm(iter, tokenVecs, type = 'dgTMatrix')
+tmDTM <-  create_dtm(iter, tokenVecs, type = 'dgTMatrix') #GG: declared as a document-term matrix.
 class(tmDTM)
-tmDTM <- as.DocumentTermMatrix(tmDTM, weighting = 'weightTf')
+tmDTM <- as.DocumentTermMatrix(tmDTM, weighting = 'weightTf') #GG: converting object if we want to use -tm-
 tmDTM
-rm(tmDTM)
+rm(tmDTM) #GG: removed because we're not gonna use it. instead we create tcm
 
 # PPTX review skip gram method
 # Create the term-cooccurence matrix; "TCM matrix usually used with GloVe word embedding model."
 tcm <- create_tcm(iter, # tokenized & normalized stings
                   tokenVecs, # function: n-gram & specific word vec construction
                   skip_grams_window = 5, # token's context is defined by 5 words before and after
-                  skip_grams_window_context = "symmetric")
+                  skip_grams_window_context = "symmetric") #GG: we want to keep terms that are shared across documents (overlap, common support)
+?create_tcm
 # Square since all retained tokens are shared between 1+ docs
 # Each element the matrix represents how often word i appears in context of word j.
 dim(tcm)
 
 # Build the GloVe model
-# instantiate the object
+# instantiate the object #GG: dimension reduction: from 68898 to 50
 fitGlove <- GlobalVectors$new(rank = 50,  # how many vectors to represent
                               x_max = 10) # max number of cooccurences; focus on 10 most frequent co-occurences
 
 # Fit the model; pg 176 in the book
+st <- Sys.time()
+set.seed(123)
 contextGlove <- fitGlove$fit_transform(tcm, 
                                    n_iter = 10, 
-                                   convergence_tol = 0.01, 
-                                   n_threads = 8)
+                                   convergence_tol = 0.01, #GG: learning rate
+                                   n_threads = 8) # Parallelize, if you have 8-core chip
 
 # According to the package author there are two vectors
 gloveContext <-  fitGlove$components
-dim(gloveContext)
+dim(gloveContext) #GG: 50 vectors with all the 68898 tokens on top
 
 # From package
 # While both of word-vectors matrices can be used as result it usually better (idea from GloVe paper) to average or take a sum of main and context vector
-wordVectors <- contextGlove + t(gloveContext)
+wordVectors <- contextGlove + t(gloveContext) #GG: package writers recommend doing this in the Glova paper
 head(wordVectors)
 
-# Find the closest word vectors for good walks in boston airbnb stays 
+# Find the closest word vectors for good walks in boston airbnb stays #GG: what represents good walk? took all token "walk"
 goodWalks <- wordVectors["walk", , drop = FALSE] - 
-  wordVectors["disappointed", , drop = FALSE] + 
+  wordVectors["disappointed", , drop = FALSE] + #GG: wanna focus on a contextualization withouth disappoinment and with good
   wordVectors["good", , drop = FALSE]
 goodWalks
 
-cosSimilarity <- sim2(x = wordVectors, 
+cosSimilarity <- sim2(x = wordVectors, #GG: now measuring the distance (through cosine similarity) between the "goodwalks" vector and the "wordVectors" huge sparse matrix
                       y = goodWalks, 
                       method = "cosine")
 cosSimilarity <- data.frame(term = rownames(cosSimilarity),
@@ -111,9 +114,9 @@ cosSimilarity <- data.frame(term = rownames(cosSimilarity),
 
 # get only the top 20 contextualized terms 
 cosSimilarity <- cosSimilarity[order(cosSimilarity$coSineSim, decreasing = T),]
-head(cosSimilarity, 20)
+head(cosSimilarity, 20) #GG: this is the context by which people in their reviews are saying they had a good walk
 
-# Find the closest word vectors for dirty sinks in boston airbnb stays 
+# Find the closest word vectors for dirty sinks in boston airbnb stays #GG: let's do the same with dirty stays
 dirtyStay <- wordVectors["dirty", , drop = FALSE] - 
   wordVectors["condition", , drop = FALSE] -
   wordVectors["clean", , drop = FALSE] 
