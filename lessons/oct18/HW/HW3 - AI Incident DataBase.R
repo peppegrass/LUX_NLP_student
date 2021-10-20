706e4811eca862a0aa3c7ce5f02515400f11a1cf
Giuseppe Grasso <peppegrass@gmail.com>
2021-10-20 13:37:04 +0200

    Create HW3 - AI Incident DataBase.R
    getting started with HW3

---
+++ lessons/oct18/HW/HW3 - AI Incident DataBase.R
@@ -0,0 +1,29 @@
+#' Title: Homework III
+#' Purpose: Obtain Data, save it, perform word frequency, then compare shared word frequencies
+#' Author: Giuseppe Grasso
+#' email: giuseppegrasso.econ@gmail.com
+#' License: GPL>=3
+#' Date: Oct 20, 2021
+#'
+
+# Preliminaries
+
+## Setting the working directory
+setwd("~/Documents/GitHub/LUX_NLP_student/lessons/oct18/HW")
+
+## Libraries
+library(skmeans)
+library(tidytext)
+library(tm)
+library(clue)
+library(cluster)
+library(wordcloud)
+library(lexicon)
+library(dplyr)
+library(plyr)
+library(radarchart)
+library(ggplot2)
+library(ggthemes)
+
+
+# End
